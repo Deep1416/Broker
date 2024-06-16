dc40cad5a3950306de8f50369614e6831e9c0930
Deependra <deependra186037@gamil.com>
2024-06-16 20:07:51 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import './App.css'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { About, Home, Profile, SignIn, SignOut } from "./pages";
+import { Header } from "./componets";
 
 function App() {
-
-
   return (
-   <div>
-    <h1 className='text-center '>
-      hello world
-    </h1>
-   </div>
-  )
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home.jsx";
+export { default as About } from "./About.jsx";
+export { default as Profile } from "./Profile.jsx";
+export { default as SignIn } from "./SignIn.jsx";
+export { default as SignOut } from "./SignOut.jsx";
